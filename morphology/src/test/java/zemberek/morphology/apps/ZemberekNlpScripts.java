1dbe60a4090d01287f4573d01d2b9116e785361a
ahmetaa <ahmetaa@gmail.com>
2016-10-30 09:25:29 +0200

    This will contain some functions for debugging etc.

---
+++ morphology/src/test/java/zemberek/morphology/apps/ZemberekNlpScripts.java
@@ -0,0 +1,32 @@
+package zemberek.morphology.apps;
+
+import org.junit.Test;
+import zemberek.morphology.lexicon.NullSuffixForm;
+import zemberek.morphology.lexicon.SuffixForm;
+import zemberek.morphology.parser.tr.TurkishWordParserGenerator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ZemberekNlpScripts {
+
+    @Test
+    public void generateSuffixNames() throws IOException {
+        TurkishWordParserGenerator parser = TurkishWordParserGenerator.createWithDefaults();
+        List<SuffixForm> forms = new ArrayList<>();
+
+        for (SuffixForm form : parser.getSuffixProvider().getAllForms()) {
+            if (form instanceof NullSuffixForm) {
+                continue;
+            }
+            forms.add(form);
+        }
+        forms.sort((a, b) -> a.getId().compareTo(b.getId()));
+        List<String> result = forms.stream().map(s -> s.id).collect(Collectors.toList());
+        Files.write(Paths.get("suffix-list"), result);
+    }
+}
