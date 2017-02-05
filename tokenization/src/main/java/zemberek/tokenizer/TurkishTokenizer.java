4b0bd3d39aa24ae40ea69489b928fd73abc2b5a1
ahmetaa <ahmetaa@gmail.com>
2017-02-05 14:02:17 +0300

    TurkishTokenizer work, not yet finished.

--- tokenization/src/main/java/zemberek/tokenizer/TurkishTokenizer.java
+++ tokenization/src/main/java/zemberek/tokenizer/TurkishTokenizer.java
@@ -4,11 +4,16 @@ package zemberek.tokenizer;
 import com.google.common.io.Resources;
 import zemberek.core.collections.FixedBitVector;
 import zemberek.core.collections.FloatValueMap;
+import zemberek.core.collections.UIntSet;
 import zemberek.core.io.IOUtil;
+import zemberek.core.logging.Log;
+import zemberek.core.text.TextUtil;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -16,24 +21,15 @@ import java.util.Locale;
  * Extracts tokens from sentences.
  * TODO: finish this.
  */
-class TurkishTokenizer implements Tokenizer {
+class TurkishTokenizer extends PerceptronSegmenter implements Tokenizer {
 
     private FloatValueMap<String> weights = new FloatValueMap<>();
 
     public static TurkishTokenizer fromInternalModel() throws IOException {
         try (DataInputStream dis = IOUtil.getDataInputStream(
                 Resources.getResource("tokenizer/sentence-boundary-model.bin").openStream())) {
-            return load(dis);
+            return new TurkishTokenizer(load(dis));
         }
-    }
-
-    private static TurkishTokenizer load(DataInputStream dis) throws IOException {
-        int size = dis.readInt();
-        FloatValueMap<String> features = new FloatValueMap<>((int) (size * 1.5));
-        for (int i = 0; i < size; i++) {
-            features.set(dis.readUTF(), dis.readFloat());
-        }
-        return new TurkishTokenizer(features);
     }
 
     private TurkishTokenizer(FloatValueMap<String> weights) {
@@ -85,12 +81,10 @@ class TurkishTokenizer implements Tokenizer {
         List<Span> tokens = new ArrayList<>();
 
         int tokenBegin = 0;
-        char previous = 0;
 
         for (int j = 0; j < sentence.length(); j++) {
             // skip if char cannot be a boundary char.
             char chr = sentence.charAt(j);
-            //if (chr==' ' || chr=='\t' || chr=='\n' || chr=='\r') {
             if (Character.isWhitespace(chr)) {
                 if (tokenBegin < j) {
                     tokens.add(new Span(tokenBegin, j));
@@ -99,7 +93,7 @@ class TurkishTokenizer implements Tokenizer {
                 continue;
             }
             if (chr < singleTokenLookup.length && singleTokenLookup.get(chr)) {
-                // add previous toke if available.
+                // add previous token if available.
                 if (tokenBegin < j) {
                     tokens.add(new Span(tokenBegin, j));
                 }
@@ -138,6 +132,175 @@ class TurkishTokenizer implements Tokenizer {
     @Override
     public List<String> tokenStrings(String input) {
         return tokenize(input);
+    }
+
+    public static class TrainerBuilder {
+        Path trainFile;
+        int iterationCount = 5;
+        int skipSpaceFrequency = 20;
+        int lowerCaseFirstLetterFrequency = 20;
+        boolean shuffleInput = false;
+
+        public TrainerBuilder(Path trainFile) {
+            this.trainFile = trainFile;
+        }
+
+        public TrainerBuilder iterationCount(int count) {
+            this.iterationCount = count;
+            return this;
+        }
+
+        public TrainerBuilder shuffleSentences() {
+            this.shuffleInput = true;
+            return this;
+        }
+
+        public TrainerBuilder skipSpaceFrequencyonCount(int count) {
+            this.skipSpaceFrequency = skipSpaceFrequency;
+            return this;
+        }
+
+        public TrainerBuilder lowerCaseFirstLetterFrequency(int count) {
+            this.lowerCaseFirstLetterFrequency = lowerCaseFirstLetterFrequency;
+            return this;
+        }
+
+        public Trainer build() {
+            return new Trainer(this);
+        }
+    }
+
+
+    public static class Trainer {
+        private TrainerBuilder builder;
+
+        public static TrainerBuilder builder(Path trainFile) {
+            return new TrainerBuilder(trainFile);
+        }
+
+        private Trainer(TrainerBuilder builder) {
+            this.builder = builder;
+        }
+
+        private static Locale Turkish = new Locale("tr");
+
+        public TurkishTokenizer train() throws IOException {
+            FloatValueMap<String> weights = new FloatValueMap<>();
+            List<String> sentences = TextUtil.loadLinesWithText(builder.trainFile);
+            FloatValueMap<String> averages = new FloatValueMap<>();
+
+            int updateCount = 0;
+
+            for (int i = 0; i < builder.iterationCount; i++) {
+
+                Log.info("Iteration = %d", i + 1);
+
+                if (builder.shuffleInput) {
+                    Collections.shuffle(sentences);
+                }
+
+                for (String sentenceWithPipe : sentences) {
+
+                    UIntSet indexSet = new UIntSet();
+
+
+                    if (sentenceWithPipe.trim().length() == 0) {
+                        continue;
+                    }
+
+                    int indexCounter = 0;
+                    for (int j = 0; j < sentenceWithPipe.length(); j++) {
+                        char chr = sentenceWithPipe.charAt(j);
+                        if (chr == '|') {
+                            indexSet.add(indexCounter);
+                        } else {
+                            indexCounter++;
+                        }
+                    }
+
+                    String sentence = sentenceWithPipe.replaceAll("\\|","");
+
+                    for (int j = 0; j < sentence.length(); j++) {
+                        // skip if char cannot be a boundary char.
+                        char chr = sentence.charAt(j);
+                        if (chr < boundaryDesicionLookup.length && boundaryDesicionLookup.get(chr)) {
+                            continue;
+                        }
+
+                        BoundaryData boundaryData = new BoundaryData(sentence, j);
+                        if (boundaryData.nonBoundaryCheck()) {
+                            continue;
+                        }
+                        List<String> features = boundaryData.extractFeatures();
+                        float score = 0;
+                        for (String feature : features) {
+                            score += weights.get(feature);
+                        }
+                        int update = 0;
+                        // if we found no-boundary but it is a boundary
+                        if (score <= 0 && indexSet.contains(j)) {
+                            update = 1;
+                        }
+                        // if we found boundary but it is not a boundary
+                        else if (score > 0 && !indexSet.contains(j)) {
+                            update = -1;
+                        }
+                        updateCount++;
+                        if (update != 0) {
+                            for (String feature : features) {
+                                double d = weights.incrementByAmount(feature, update);
+                                if (d == 0.0) {
+                                    weights.remove(feature);
+                                }
+                                d = averages.incrementByAmount(feature, updateCount * update);
+                                if (d == 0.0) {
+                                    averages.remove(feature);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (String key : weights) {
+                weights.set(key, weights.get(key) - averages.get(key) * 1f / updateCount);
+            }
+
+            return new TurkishTokenizer(weights);
+        }
+
+    }
+
+    static class BoundaryData {
+        char currentChar;
+        char previousLetter;
+        char nextLetter;
+        String previousTwoLetters;
+        String nextTwoLetters;
+
+        BoundaryData(String input, int pointer) {
+
+            previousLetter = pointer > 0 ? input.charAt(pointer - 1) : '_';
+            nextLetter = pointer < input.length() - 1 ? nextLetter = input.charAt(pointer + 1) : '_';
+            previousTwoLetters = pointer > 2 ? input.substring(pointer - 2, pointer) : "__";
+            nextTwoLetters = pointer < input.length() - 3 ? input.substring(pointer + 1, pointer + 3) : "__";
+            currentChar = input.charAt(pointer);
+        }
+
+
+        boolean nonBoundaryCheck() {
+            return false;
+        }
+
+        List<String> extractFeatures() {
+            List<String> features = new ArrayList<>();
+            features.add("1:" + Character.isUpperCase(previousLetter));
+            features.add("1b:" + Character.isWhitespace(nextLetter));
+            features.add("1a:" + previousLetter);
+            features.add("1b:" + nextLetter);
+            features.add("2p:" + previousTwoLetters);
+            features.add("2n:" + nextTwoLetters);
+            return features;
+        }
     }
 
 
