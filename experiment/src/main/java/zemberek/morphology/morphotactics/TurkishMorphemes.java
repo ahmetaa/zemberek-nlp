3ef34f599191921bfdc0a718298c672406fa0e9c
ahmetaa <ahmetaa@gmail.com>
2017-02-06 19:03:06 +0300

    morphotactics continue

--- experiment/src/main/java/zemberek/morphology/morphotactics/TurkishMorphemes.java
+++ experiment/src/main/java/zemberek/morphology/morphotactics/TurkishMorphemes.java
@@ -37,23 +37,26 @@ public class TurkishMorphemes {
 
     // ------------- Transitions ---------------------------------
 
-    MorphemeTransition noun_a3sg_eps = transition(noun, a3sg, ""); // ev-ε
+    MorphemeTransition noun_a3sg_eps = transition(noun, a3sg, ""); // ev-ε-?-?
 
-    MorphemeTransition a3sg_pnon_eps = transition(a3sg, pnon, ""); // ev-ε-ε
-
+    MorphemeTransition a3sg_pnon_eps = transition(a3sg, pnon, ""); // ev-ε-ε-?
     MorphemeTransition a3sg_p1sg_Im = transition(a3sg, p1sg, "+Im"); //ev-ε-im oda-ε-m
-    MorphemeTransition a3sg_p1sg_yIm = transition(a3sg, p1sg, "+yIm"); //su-ε-yum
-    
+    MorphemeTransition a3sg_p1sg_yIm = transition(a3sg, p1sg, "+yIm"); //su-ε-yum. Only for "su"
     MorphemeTransition a3sg_p2sg_In = transition(a3sg, p2sg, "+In"); //ev-ε-in oda-ε-n
-    MorphemeTransition a3sg_p2sg_yIn = transition(a3sg, p2sg, "+yIn"); //su-ε-yun
+    MorphemeTransition a3sg_p2sg_yIn = transition(a3sg, p2sg, "+yIn"); //su-ε-yun Only for "su"
+
+    MorphemeTransition a1pl_pnon_lAr = transition(a1pl, pnon, "lAr"); // ev-ler-ε-?
+    MorphemeTransition a1pl_p1sg_Im = transition(a1pl, p1sg, "+Im"); //ev-ler-im oda-lar-ım
+    MorphemeTransition a1pl_p2sg_In = transition(a1pl, p2sg, "+In"); //ev-ler-in oda-lar-ın
 
     MorphemeTransition pnon_nom_eps = transition(pnon, nom, ""); //ev-?-ε-ε (ev, evler)
-    MorphemeTransition p1sg_nom_eps = transition(p1sg, nom, ""); //ev-?-im-ε (evim, evlerim)
-    MorphemeTransition p2sg_nom_eps = transition(p2sg, nom, ""); //ev-?-in-ε (evin, evlerin)
-
     MorphemeTransition pnon_dat_yA = transition(pnon, dat, "+yA"); //ev-?-ε-e (eve, evlere)
     MorphemeTransition pnon_dat_eps = transition(pnon, dat, ""); //içeri-ε-ε (Only for some words)
+
+    MorphemeTransition p1sg_nom_eps = transition(p1sg, nom, ""); //ev-?-im-ε (evim, evlerim)
     MorphemeTransition p1sg_dat_yA = transition(p1sg, dat, "+yA"); //ev-?-in-e (evime, evlerime)
+
+    MorphemeTransition p2sg_nom_eps = transition(p2sg, nom, ""); //ev-?-in-ε (evin, evlerin)
     MorphemeTransition p2sg_dat_yA = transition(p2sg, dat, "+yA"); //ev-?-in-e (evine, evlerine)
 
     private static MorphemeState morpheme(String id) {
@@ -63,7 +66,11 @@ public class TurkishMorphemes {
     private static MorphemeTransition transition(MorphemeState from, MorphemeState to, String format) {
         return new MorphemeTransition(from, to, format);
     }
-    
+
+    enum TransitionRule {
+        ALLOW_ONLY,
+        ALLOW_EXCEPT
+    }
 
     
 
