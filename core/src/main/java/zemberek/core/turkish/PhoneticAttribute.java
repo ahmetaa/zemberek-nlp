c17400497b061d50d5c7cd474363b17e9ded5e84
Ahmet <ahmetaa@gmail.com>
2017-11-17 09:42:45 +0300

    Documentation

--- core/src/main/java/zemberek/core/turkish/PhoneticAttribute.java
+++ core/src/main/java/zemberek/core/turkish/PhoneticAttribute.java
@@ -5,17 +5,33 @@ import zemberek.core.enums.StringEnum;
 import zemberek.core.enums.StringEnumMap;
 
 public enum PhoneticAttribute implements StringEnum {
+
+    // Turkish vowels are: [a, e, ı, i, o, ö, u, ü]
     LastLetterVowel("LLV"),
+
+    // Turkish consonants are: [b, c, ç, d, f, g, ğ, h, j, k, l, m, n, p, r, s, ş, t, v, y, z]
     LastLetterConsonant("LLC"),
 
+    // Turkish Frontal vowels are: [e, i, ö, ü]
     LastVowelFrontal("LVF"),
+
+    // Back vowels are: [a, ı, o, u]
     LastVowelBack("LVB"),
+
+    // Rounded wovels are: [o, u, ö, ü]
     LastVowelRounded("LVR"),
+
+    // Unrounded wovels are: [a, e, ı, i]
     LastVowelUnrounded("LVuR"),
 
+    // Turkish voiceless consonants are [ç, f, h, k, p, s, ş, t]
     LastLetterVoiceless("LLVless"),
+
+    // Turkish voiced consonants are [b, c, d, g, ğ, h, j, l, m, n, r, v, y, z]
     LastLetterNotVoiceless("LLNotVless"),
 
+    // Turkish Voiceless stop consonants are: [ç, k, p, t]. Voiced stop consonants are [b, c, d, g, ğ]
+    // TODO: short form should be LLVlessStop
     LastLetterVoicelessStop("LLStop"),
 
     FirstLetterVowel("FLV"),
