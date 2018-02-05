ff36aa7e5fabc36c69425e247289b2ce147389c3
aaa <ahmetaa@gmail.com>
2018-02-05 11:41:08 +0300

    implicit dative test.

--- experiment/src/test/java/zemberek/morphology/analysis/InterpretingAnalyzerFunctionalTest.java
+++ experiment/src/test/java/zemberek/morphology/analysis/InterpretingAnalyzerFunctionalTest.java
@@ -5,22 +5,48 @@ import org.junit.Assert;
 import org.junit.Test;
 import zemberek.morphology.analyzer.AnalysisResult;
 import zemberek.morphology.analyzer.InterpretingAnalyzer;
+import zemberek.morphology.analyzer.MorphemeSurfaceForm;
 import zemberek.morphology.lexicon.RootLexicon;
 import zemberek.morphology.lexicon.tr.TurkishDictionaryLoader;
 
 public class InterpretingAnalyzerFunctionalTest {
 
+  public static InterpretingAnalyzer getAnalyzer(String... dictionaryLines) {
+    RootLexicon loader = new TurkishDictionaryLoader().load(dictionaryLines);
+    return new InterpretingAnalyzer(loader);
+  }
+
+  public boolean containsMorpheme(AnalysisResult result, String morphemeName) {
+    for (MorphemeSurfaceForm forms : result.getMorphemes()) {
+      if (forms.lexicalTransition.to.morpheme.id.equalsIgnoreCase(morphemeName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
   @Test
   public void shouldParse_1() {
-
-    RootLexicon loader = new TurkishDictionaryLoader().load("elma");
-    InterpretingAnalyzer analyzer = new InterpretingAnalyzer(loader);
-    List<AnalysisResult> results = analyzer.analyze("elmalar");
+    List<AnalysisResult> results = getAnalyzer("elma").analyze("elmalar");
     Assert.assertEquals(1, results.size());
     AnalysisResult first = results.get(0);
-    System.out.println("Parse result = "  + first);
+    System.out.println("Parse result = " + first);
     Assert.assertEquals("elma_Noun", first.getDictionaryItem().id);
-    Assert.assertEquals("elma", first.root);
+    Assert.assertTrue(containsMorpheme(first, "A3pl"));
   }
+
+  @Test
+  public void implicitDative_1() {
+    List<AnalysisResult> results = getAnalyzer("içeri [A:ImplicitDative]")
+        .analyze("içeri");
+    Assert.assertEquals(2, results.size());
+    AnalysisResult first = results.get(1);
+    System.out.println("Parse result = " + first);
+    Assert.assertEquals("içeri_Noun", first.getDictionaryItem().id);
+    Assert.assertEquals("içeri", first.root);
+    Assert.assertTrue(containsMorpheme(first, "Dat"));
+  }
+
 
 }
