25128c3794c5205a24e5812cd6d40c450624a380
aaa <ahmetaa@gmail.com>
2018-03-05 15:10:43 +0300

    more causative tests

--- experiment/src/test/java/zemberek/morphology/analysis/VerbVerbDerivationTest.java
+++ experiment/src/test/java/zemberek/morphology/analysis/VerbVerbDerivationTest.java
@@ -14,6 +14,28 @@ public class VerbVerbDerivationTest extends AnalyzerTestBase {
         matchesTailLex("Verb + Caus + Verb + Caus + Verb + Imp + A2sg"));
     tester.expectSingle("okutturt",
         matchesTailLex("Verb + Caus + Verb + Caus + Verb + Caus + Verb + Imp + A2sg"));
+
+    tester.expectFail(
+        "okutt",
+        "okuturtur"
+    );
+
+    // "okutur" should not have a causative analysis
+    tester.expectFalse("okutur", matchesTailLex("Verb + Caus + Verb + Imp + A2sg"));
+
+    tester = getTester("semirmek");
+    tester.expectSingle("semirt",
+        matchesTailLex("Verb + Caus + Verb + Imp + A2sg"));
+    tester.expectSingle("semirttir",
+        matchesTailLex("Verb + Caus + Verb + Caus + Verb + Imp + A2sg"));
+    tester.expectSingle("semirttirt",
+        matchesTailLex("Verb + Caus + Verb + Caus + Verb + Caus + Verb + Imp + A2sg"));
+
+    tester.expectFail(
+        "semirtt",
+        "semirtirtir"
+    );
+
   }
 
 }
