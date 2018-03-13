2c5c7ebec436a2bf90319f48a41ab1c562d22439
aaa <ahmetaa@gmail.com>
2018-03-13 13:10:47 +0300

    multi threaded coverage test

--- morphology/src/test/java/zemberek/morphology/_analyzer/CoverageTest.java
+++ morphology/src/test/java/zemberek/morphology/_analyzer/CoverageTest.java
@@ -3,16 +3,19 @@ package zemberek.morphology._analyzer;
 import com.google.common.base.Stopwatch;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
@@ -26,45 +29,89 @@ public class CoverageTest {
 
   @Test
   @Ignore(value = "Coverage Test.")
-  public void testCoverage() throws IOException {
+  public void testCoverage() throws Exception {
     String path = "../data/zemberek-oflazer/oflazer-zemberek-parsed.txt.gz";
-    Log.info("Extracting coverage test file: %s" , path);
+    Log.info("Extracting coverage test file: %s", path);
     Path oflazerAndZemberek = Paths.get(path);
     InputStream gzipStream = new GZIPInputStream(new FileInputStream(oflazerAndZemberek.toFile()));
     BufferedReader reader = new BufferedReader(
         new InputStreamReader(gzipStream, StandardCharsets.UTF_8));
 
-    LinkedHashSet<String> lines = reader.lines()
-        .collect(Collectors.toCollection(LinkedHashSet::new));
+    ArrayDeque<String> lines = reader.lines()
+        .collect(Collectors.toCollection(ArrayDeque::new));
     Log.info("File read, analyzing.");
     RootLexicon lexicon = TurkishDictionaryLoader.loadDefaultDictionaries();
     InterpretingAnalyzer analyzer = new InterpretingAnalyzer(lexicon);
 
+    int threadCount = Runtime.getRuntime().availableProcessors() / 2;
+    ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+    CompletionService<Result> service = new ExecutorCompletionService<>(executorService);
+
     List<String> failedWords = new ArrayList<>();
 
-    int i = 0;
     Stopwatch sw = Stopwatch.createStarted();
-    for (String line : lines) {
-      List<AnalysisResult> results = analyzer.analyze(line);
-      if (results.size() == 0) {
-        failedWords.add(line);
+
+    int batchCount = 0;
+    int batchSize = 20_000;
+
+    while (!lines.isEmpty()) {
+      List<String> batch = new ArrayList<>();
+      int j = 0;
+      while (j < batchSize && !lines.isEmpty()) {
+        batch.add(lines.poll());
+        j++;
       }
-      i++;
-      if (i % 100_000 == 0) {
-        logResult(failedWords, i, sw);
+
+      if (batch.size() > 0) {
+        service.submit(() -> {
+          List<String> failed = new ArrayList<>();
+          for (String s : batch) {
+            List<AnalysisResult> results = analyzer.analyze(s);
+            if (results.size() == 0) {
+              failed.add(s);
+            }
+          }
+          return new Result(failed, batch.size());
+        });
+        batchCount++;
       }
     }
-    logResult(failedWords, lines.size(), sw);
+
+    int i = 0;
+    int total = 0;
+    while (i < batchCount) {
+      Result r = service.take().get();
+      failedWords.addAll(r.failed);
+      total += r.wordCount;
+      if (total % (batchSize * 10) == 0) {
+        logResult(failedWords, total, sw);
+      }
+      i++;
+    }
+
+    logResult(failedWords, total, sw);
     Files.write(
         Paths.get("../data/zemberek-oflazer/new-analyzer-failed.txt"),
         failedWords, StandardCharsets.UTF_8);
+  }
+
+  class Result {
+
+    List<String> failed = new ArrayList<>();
+    int wordCount;
+
+    Result(List<String> failed, int wordCount) {
+      this.failed = failed;
+      this.wordCount = wordCount;
+    }
   }
 
   private void logResult(List<String> failedWords, int i, Stopwatch sw) {
     double coverage = 100 - (failedWords.size() * 100d / i);
     double seconds = sw.elapsed(TimeUnit.MILLISECONDS) / 1000d;
     double speed = i / seconds;
-    Log.info("%d analysed. Coverage = %.3f . Speed = %.3f tokens/sec", i, coverage, speed);
+    Log.info("Elapsed %.2f sec. %d analysed. Coverage = %.3f . Speed = %.3f tokens/sec",
+        seconds, i, coverage, speed);
   }
 
 
