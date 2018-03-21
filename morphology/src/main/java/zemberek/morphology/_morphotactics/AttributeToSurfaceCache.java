7b2255dcd2d2c41f2570c767ccee54dde0e0d9ef
Mehmet <mdakin@gmail.com>
2018-03-21 15:31:00 +0100

    Add some doc to cache.

--- morphology/src/main/java/zemberek/morphology/_morphotactics/AttributeToSurfaceCache.java
+++ morphology/src/main/java/zemberek/morphology/_morphotactics/AttributeToSurfaceCache.java
@@ -2,6 +2,17 @@ package zemberek.morphology._morphotactics;
 
 import java.util.Arrays;
 
+/**
+ * A cache for set of morphemic attributes to surface forms.
+ *
+ * For thread safety, writes to map are synchronized, when map needs expanding
+ * writer thread creates the expanded version and replaces the map. The map
+ * reference is volatile so readers always see a consistent albeit possibly
+ * stale version of the map.
+ *
+ * This is also knows as cheap read-write lock trick (see item #5 in the link)
+ * https://www.ibm.com/developerworks/java/library/j-jtp06197/index.html
+ */
 public class AttributeToSurfaceCache {
   private static final int DEFAULT_INITIAL_CAPACITY = 4;
 
