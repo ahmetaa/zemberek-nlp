f224ebfe8380d0daa68126bc8694fcb97bd07111
Mehmet <mdakin@gmail.com>
2018-04-03 14:20:10 +0200

    Semaphore is unnecessary.

--- morphology/src/main/java/zemberek/morphology/_analyzer/AnalysisCache.java
+++ morphology/src/main/java/zemberek/morphology/_analyzer/AnalysisCache.java
@@ -6,7 +6,6 @@ import com.google.common.base.Stopwatch;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import zemberek.core.logging.Log;
@@ -23,7 +22,6 @@ class AnalysisCache {
 
   private static final String MOST_USED_WORDS_FILE = "/tr/first-10K";
   private ConcurrentHashMap<String, _WordAnalysis> staticCache;
-  private Semaphore staticCacheSemaphore = new Semaphore(1);
   private boolean staticCacheInitialized = false;
   private long staticCacheHits;
   private long staticCacheMiss;
@@ -46,37 +44,28 @@ class AnalysisCache {
     staticCache = new ConcurrentHashMap<>(STATIC_CACHE_CAPACITY);
   }
 
-  public void initializeStaticCache(Function<String, _WordAnalysis> analysisProvider) {
-    try {
-      staticCacheSemaphore.acquire();
-      // Some other thread already initiated priming of cache, bail out.
-      if (staticCacheInitialized) {
-        return;
-      }
-      new Thread(() -> {
-        try {
-          Stopwatch stopwatch = Stopwatch.createStarted();
-          List<String> words = TextIO.loadLinesFromResource(MOST_USED_WORDS_FILE);
-          Log.info("File read in %d ms.", stopwatch.elapsed(TimeUnit.MILLISECONDS));
-          // TODO(make configurable)
-          int size = Math.min(STATIC_CACHE_CAPACITY, words.size());
-          for (int i = 0; i < size; i++) {
-            String word = words.get(i);
-            staticCache.put(word, analysisProvider.apply(word));
-          }
-          Log.info("Static cache initialized with %d most frequent words", size);
-          Log.info("Initialization time: %d ms.", stopwatch.elapsed(TimeUnit.MILLISECONDS));
-        } catch (IOException e) {
-          Log.error("Could not read most frequent words list, static cache is disabled.");
-          e.printStackTrace();
-        }
-      }).start();
-      staticCacheInitialized = true;
-    } catch (Exception e) {
-      Log.exception(e);
-    } finally {
-      staticCacheSemaphore.release();
+  public synchronized void initializeStaticCache(Function<String, _WordAnalysis> analysisProvider) {
+    if (staticCacheInitialized) {
+      return;
     }
+    new Thread(() -> {
+      try {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        List<String> words = TextIO.loadLinesFromResource(MOST_USED_WORDS_FILE);
+        Log.info("File read in %d ms.", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+        int size = Math.min(STATIC_CACHE_CAPACITY, words.size());
+        for (int i = 0; i < size; i++) {
+          String word = words.get(i);
+          staticCache.put(word, analysisProvider.apply(word));
+        }
+        Log.info("Static cache initialized with %d most frequent words", size);
+        Log.info("Initialization time: %d ms.", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+      } catch (IOException e) {
+        Log.error("Could not read most frequent words list, static cache is disabled.");
+        e.printStackTrace();
+      }
+    }).start();
+    staticCacheInitialized = true;
   }
 
   public _WordAnalysis getAnalysis(String input, Function<String, _WordAnalysis> analysisProvider) {
