10e9200445f29116b68bb109633a23e58b702ddf
aaa <ahmetaa@gmail.com>
2018-05-07 13:21:26 +0300

    Some docs

--- morphology/src/main/java/zemberek/morphology/analysis/AnalysisFormatters.java
+++ morphology/src/main/java/zemberek/morphology/analysis/AnalysisFormatters.java
@@ -11,21 +11,89 @@ import zemberek.morphology.lexicon.DictionaryItem;
 
 public class AnalysisFormatters {
 
+  /**
+   * Default morphological Analysis formatter.
+   * Pipe `|` represents derivation boundary.
+   * Left side of `→` represents derivation causing morpheme, right side represents the derivation
+   * result.
+   * <pre>
+   * kitap -> [kitap:Noun] kitap:Noun+A3s
+   * kitaplarda -> [kitap:Noun] kitap:Noun+lar:A3pl+da:Loc
+   * okut -> [okumak:Verb] oku:Verb|t:Caus→Verb+Imp+A2sg
+   * </pre>
+   */
   public static AnalysisFormatter DEFAULT = DefaultFormatter.surfaceAndLexical();
+
+  /**
+   * Default lexical morphological analysis formatter.
+   * <pre>
+   * kitap -> [kitap:Noun] Noun+A3s
+   * kitaplarda -> [kitap:Noun] Noun+A3pl+Loc
+   * okut -> [okumak:Verb] Verb|Caus→Verb+Imp+A2sg
+   * </pre>
+   */
   public static AnalysisFormatter DEFAULT_LEXICAL = DefaultFormatter.onlyLexical();
+
+  /**
+   * Default lexical morphological analysis formatter. But it will not contain
+   * Dictionary item related data.
+   * <pre>
+   * kitap -> Noun+A3s
+   * kitaplarda -> Noun+A3pl+Loc
+   * okut -> Verb|Caus→Verb+Imp+A2sg
+   * </pre>
+   */
   public static AnalysisFormatter DEFAULT_LEXICAL_ONLY_MORPHEMES = MorphemesFormatter.lexical();
+
+  /**
+   * Format's analysis result similar to tools developed by Kemal Oflazer. However, this does not
+   * generate exactly same outputs as morpheme names and morphotactics are slightly different. For
+   * example output will not contain "Pnon" or "Nom" morphemes.
+   * <pre>
+   *   kitaplarda -> kitap+Noun+A3pl+Loc
+   *   kitapsız -> kitap+Noun+A3sg^DB+Adj+Without
+   *   kitaplardaymış -> kitap+Noun+A3pl+Loc^DB+Verb+Zero+Narr+A3sg   *
+   * </pre>
+   */
   public static AnalysisFormatter OFLAZER_STYLE = new OflazerStyleFormatter();
+
+  /**
+   * Generates " + " separated Morpheme ids. Such as:
+   * <pre>
+   *   kitap -> Noun + A3sg
+   *   kitaba -> Noun + A3sg + Dat
+   *   kitapçığa -> Noun + A3sg + Dim + Noun + A3sg + Dat
+   * </pre>
+   */
   public static AnalysisFormatter LEXICAL_SEQUENCE = lexicalSequenceFormatter();
+
+  /**
+   * Generates " + " separated Surface forms and Morpheme ids:
+   * <pre>
+   *   kitap -> kitap:Noun + A3sg
+   *   kitaba -> kitab:Noun + A3sg + a:Dat
+   *   kitapçığa -> kitap:Noun + A3sg + çığ:Dim + Noun + A3sg + a:Dat
+   * </pre>
+   */
   public static AnalysisFormatter SURFACE_AND_LEXICAL_SEQUENCE = surfaceSequenceFormatter();
+
+  /**
+   * Generates space separated surface forms. Such as:
+   * <pre>
+   *   kitap -> kitap
+   *   kitaba -> kitab a
+   *   kitabımızdan -> kitab ımız dan
+   * </pre>
+   */
   public static AnalysisFormatter SURFACE_SEQUENCE = new OnlySurfaceFormatter();
 
-  public static AnalysisFormatter lexicalSequenceFormatter() {
+  static AnalysisFormatter lexicalSequenceFormatter() {
     return analysis -> String.join(" + ", analysis.getMorphemeDataList().stream()
         .map(s -> s.morpheme.id)
         .collect(Collectors.toList()));
   }
 
-  public static AnalysisFormatter surfaceSequenceFormatter() {
+  static AnalysisFormatter surfaceSequenceFormatter() {
     return analysis ->
         String.join(" + ", analysis.getMorphemeDataList().stream()
             .map(MorphemeData::toMorphemeString)
@@ -138,7 +206,7 @@ public class AnalysisFormatters {
       return new DefaultFormatter(true);
     }
 
-    public DefaultFormatter(boolean addSurface) {
+    DefaultFormatter(boolean addSurface) {
       this.addSurface = addSurface;
       morphemesFormatter = addSurface ? MorphemesFormatter.surface() :
           MorphemesFormatter.lexical();
