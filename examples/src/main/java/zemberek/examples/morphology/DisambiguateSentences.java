72eb7466c7a7d5cd5a9a173a4b1ea9f4563bc49e
ahmetaa <ahmetaa@gmail.com>
2018-06-27 23:14:48 +0300

    revert example file to original.

--- examples/src/main/java/zemberek/examples/morphology/DisambiguateSentences.java
+++ examples/src/main/java/zemberek/examples/morphology/DisambiguateSentences.java
@@ -1,26 +1,32 @@
 package zemberek.examples.morphology;
 
-import java.io.IOException;
 import java.util.List;
 import zemberek.core.logging.Log;
-import zemberek.morphology.analysis.SentenceAnalysis;
-import zemberek.morphology.analysis.SentenceWordAnalysis;
-import zemberek.morphology.analysis.SingleAnalysis;
 import zemberek.morphology.TurkishMorphology;
+import zemberek.morphology.analysis.SentenceAnalysis;
+import zemberek.morphology.analysis.SingleAnalysis;
 import zemberek.morphology.analysis.WordAnalysis;
 
 public class DisambiguateSentences {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
 
     TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+
     String sentence = "Yarın akşam kar yağacak gibi.";
+    Log.info("Sentence  = " + sentence);
+    List<WordAnalysis> analyses = morphology.analyzeSentence(sentence);
 
-    List<WordAnalysis> parses = morphology.analyzeSentence(sentence);
-    SentenceAnalysis result = morphology.disambiguate(sentence, parses);
-
-    for(SingleAnalysis a : result.bestAnalysis()) {
-      System.out.println(a);
+    Log.info("Sentence word analysis result:");
+    for (WordAnalysis entry : analyses) {
+      Log.info("Word = " + entry.getInput());
+      for (SingleAnalysis analysis : entry) {
+        Log.info(analysis.formatLong());
+      }
     }
+    SentenceAnalysis result = morphology.disambiguate(sentence, analyses);
+
+    Log.info("\nAfter ambiguity resolution : ");
+    result.bestAnalysis().forEach(Log::info);
   }
 }
