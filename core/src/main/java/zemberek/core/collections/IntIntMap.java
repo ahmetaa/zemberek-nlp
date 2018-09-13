7b2779ca124ad7cbca2e17580e85a8b96ad91949
Mehmet <mdakin@gmail.com>
2018-09-13 14:20:02 +0200

    Some random documentation fixes for IntIntMap

--- core/src/main/java/zemberek/core/collections/IntIntMap.java
+++ core/src/main/java/zemberek/core/collections/IntIntMap.java
@@ -3,40 +3,47 @@ package zemberek.core.collections;
 import java.util.Arrays;
 
 /**
- * A simple hashmap with integer keys integer values. implements open address linear probing
+ * A simple hashmap with integer keys and integer values. Implements open address linear probing
  * algorithm. Constraints: <pre>
  * - Supports int key values in range (Integer.MIN_VALUE..Integer.MAX_VALUE];
  * - Does not implement Map interface
  * - Capacity can be max 1 << 28
+ * - Max size is capacity * LOAD_FACTOR = ~175M elements (For 0.65 load factor)
  * - Does not implement Iterable.
  * - Class is not thread safe.
  * </pre>
  */
 public final class IntIntMap {
-
   public static final int NO_RESULT = Integer.MIN_VALUE;
   private static final int DEFAULT_INITIAL_CAPACITY = 4;
-  /**
-   * Capacity of the map is expanded when size reaches to capacity * LOAD_FACTOR. This value is
-   * selected to fit max 5 elements to 8 and 10 elements to a 16 sized map.
-   */
+  // Capacity of the map is expanded when size reaches to capacity * LOAD_FACTOR. This value is
+  // selected to fit max 5 elements to 8 and 10 elements to a 16 sized map.
   private static final float LOAD_FACTOR = 0.65f;
   private static final int MAX_CAPACITY = 1 << 28;
   // Special value to mark empty cells.
   private static final int EMPTY = Integer.MIN_VALUE;
-  // Backing array for keys and values.
+  // Backing array for keys and values. key and value pairs are put next to each other instead of 2
+  // separate arrays for better caching behavior.
   private int[] entries;
-
   // Number of keys in the map = size of the map.
   private int keyCount;
-
   // When size reaches a threshold, backing arrays are expanded.
   private int threshold;
 
   /**
    * Map capacity is always a power of 2. With this property, integer modulo operation (key %
-   * capacity) can be replaced with (key & (capacity - 1)) We keep (capacity - 1) value in this
-   * variable.
+   * capacity) can be replaced with (key & (capacity - 1)). We keep (capacity - 1) value in this
+   * variable. Because keys and values are always put next to each other, we keep
+   * the (capacity * 2 - 1) in a separate variable. First modulo is used to track logical array
+   * of key-value pairs second one operates on the whole array.
+   * <p>First modulo is used to find the index of the key-value pair given a hash value with:
+   * <p>
+   * slot = (hash(key) & modulo) << 1;  equivalent to  slot = (hash(key) % capacity) / 2
+   * <p>
+   * second one is used for linearly walking on the array when moving to next pair:
+   * <p>
+   * next_slot = (slot + 2) & modulo2;  equivalent to next_slot = (slot + 2) % size
+   * <p>
    */
   private int modulo;
   private int modulo2;
@@ -46,13 +53,15 @@ public final class IntIntMap {
   }
 
   /**
-   * @param capacity initial internal array size. It must be a positive number. If value is not a
-   * power of two, size will be the nearest larger power of two.
+   * @param capacity initial internal array size for capacity amount of key - values.
+   * It must be a positive number. If value is not a power of two, size will be the nearest
+   * larger power of two.
    */
   @SuppressWarnings("unchecked")
   public IntIntMap(int capacity) {
     // Backing array has always size = capacity * 2
     capacity = adjustInitialCapacity(capacity);
+    // key value pairs are kept next to each other
     entries = new int[capacity * 2];
     Arrays.fill(entries, EMPTY);
     modulo = capacity - 1;
@@ -156,7 +165,7 @@ public final class IntIntMap {
   }
 
   /**
-   * @return The array of keys in the map.
+   * @return The array of keys in the map. Not ordered.
    */
   public int[] getKeys() {
     int[] keyArray = new int[keyCount];
@@ -170,7 +179,7 @@ public final class IntIntMap {
   }
 
   /**
-   * @return The array of keys in the map.
+   * @return The array of values in the map. Not ordered.
    */
   public int[] getValues() {
     int[] valueArray = new int[keyCount];
@@ -183,7 +192,6 @@ public final class IntIntMap {
   }
 
   private int locate(int key) {
-    // key location is the hash
     int slot = (rehash(key) & modulo) << 1;
     while (true) {
       final int k = entries[slot];
