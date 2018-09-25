ead746d275518b268ec1d85acaba22bded47548e
ahmetaa <ahmetaa@gmail.com>
2018-09-25 11:53:47 +0300

    grpc experiment

--- grpc/src/main/php/proto_gen.sh
+++ grpc/src/main/php/proto_gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 protoc --proto_path=./../proto   --php_out=./   --grpc_out=./   --plugin=protoc-gen-grpc=/home/ahmetaa/projects/grpc/bins/opt/grpc_php_plugin   ./../proto/language_id.proto
+protoc --proto_path=./../proto   --php_out=./   --grpc_out=./   --plugin=protoc-gen-grpc=/home/ahmetaa/projects/grpc/bins/opt/grpc_php_plugin   ./../proto/lemmatization.proto
 
 
