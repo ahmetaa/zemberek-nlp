e748692e290b0add26b0c506572d61f045abc9a2
aaa <ahmetaa@gmail.com>
2018-09-26 18:22:29 +0300

    private Token class.

---
+++ tokenization/src/main/java/zemberek/tokenization/_Token.java
@@ -0,0 +1,57 @@
+package zemberek.tokenization;
+
+class _Token {
+  public final String content;
+  public final Type type;
+  public final int start;
+  public final int end;
+
+  public _Token(String content, Type type, int start, int end) {
+    this.content = content;
+    this.type = type;
+    this.start = start;
+    this.end = end;
+  }
+
+  enum Type {
+
+    // white space
+    SpaceTab,
+    NewLine,
+
+    // words
+    Word,
+    WordAlphanumerical,
+    WordWithSymbol,
+    Abbreviation,
+    AbbreviationWithDots,
+
+    Punctuation,
+
+    // numerals
+    RomanNumeral,
+    RatioNumeral,
+    RangeNumeral,
+    RealNumeral,
+    DistributionNumeral,
+    OrdinalNumeral,
+    CardinalNumeral,
+    PercentNumeral,
+
+    // temporal
+    Time,
+    Date,
+
+    // web related
+    URL,
+    Email,
+    HashTag,
+    Mention,
+
+    Emoji,
+    Emoticon,
+
+    UnknownWord,
+    Unknown,
+  }
+}
