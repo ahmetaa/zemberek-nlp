a0b40fb51aa176cec2b1da0834070e962f0f357a
aaa <ahmetaa@gmail.com>
2018-09-30 15:01:46 +0300

    Start writing changelog for 0.16.0

--- apps/src/main/java/zemberek/apps/morphology/MorphologyConsole.java
+++ apps/src/main/java/zemberek/apps/morphology/MorphologyConsole.java
@@ -1,6 +1,7 @@
 package zemberek.apps.morphology;
 
 import com.beust.jcommander.Parameter;
+import java.io.IOException;
 import java.util.Scanner;
 import zemberek.apps.ConsoleApp;
 import zemberek.morphology.TurkishMorphology;
@@ -9,6 +10,9 @@ import zemberek.morphology.analysis.SentenceAnalysis;
 import zemberek.morphology.analysis.SentenceWordAnalysis;
 import zemberek.morphology.analysis.SingleAnalysis;
 import zemberek.morphology.analysis.WordAnalysis;
+import zemberek.morphology.lexicon.DictionarySerializer;
+import zemberek.morphology.lexicon.RootLexicon;
+import zemberek.morphology.morphotactics.InformalTurkishMorphotactics;
 
 public class MorphologyConsole extends ConsoleApp {
 
@@ -16,6 +20,10 @@ public class MorphologyConsole extends ConsoleApp {
       description = "If used, unknown words will not be analysed. Such as out of dictionary "
           + "Proper nouns or numerals.")
   public boolean disableUnknownAnalysis;
+
+  @Parameter(names = {"--enableInformalWordAnalysis", "-informal"},
+      description = "If used, informal word analysis results will be included.")
+  public boolean enableInformalWordAnalysis;
 
   public static void main(String[] args) {
     new MorphologyConsole().execute(args);
@@ -27,12 +35,16 @@ public class MorphologyConsole extends ConsoleApp {
   }
 
   @Override
-  public void run() {
+  public void run() throws IOException {
     Builder b = TurkishMorphology.builder();
-    b.addDefaultBinaryDictionary();
     if (disableUnknownAnalysis) {
       b.disableUnidentifiedTokenAnalyzer();
     }
+    RootLexicon lexicon = DictionarySerializer.loadFromResources("/tr/lexicon.bin");
+    if (enableInformalWordAnalysis) {
+      b.morphotactics(new InformalTurkishMorphotactics(lexicon));
+    }
+    b.useLexicon(lexicon);
     TurkishMorphology morphology = b.build();
     String input;
     System.out.println("Enter word or sentence. Type `quit` or `Ctrl+C` to exit.:");
