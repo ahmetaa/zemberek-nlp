2a6bdf605335a2c174bf8174c84ce8242824b105
ahmet@peak.com <ahmet@peak.com>
2019-08-02 07:20:47 +0300

    Add verb generation example.

--- examples/src/main/java/zemberek/examples/morphology/GenerateWords.java
+++ examples/src/main/java/zemberek/examples/morphology/GenerateWords.java
@@ -1,13 +1,25 @@
 package zemberek.examples.morphology;
 
-import java.util.List;
 import zemberek.morphology.TurkishMorphology;
 import zemberek.morphology.generator.WordGenerator.Result;
 import zemberek.morphology.lexicon.DictionaryItem;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class GenerateWords {
 
   public static void main(String[] args) {
+
+    generateNouns();
+    System.out.println();
+    generateVerbs();
+  }
+
+  private static void generateNouns() {
+
+    System.out.println("Generating Nouns.");
 
     String[] number = {"A3sg", "A3pl"};
     String[] possessives = {"P1sg", "P2sg", "P3sg"};
@@ -22,6 +34,33 @@ public class GenerateWords {
         for (String caseM : cases) {
           List<Result> results =
               morphology.getWordGenerator().generate(item, numberM, possessiveM, caseM);
+          results.forEach(s->System.out.println(s.surface));
+        }
+      }
+    }
+  }
+
+
+  private static void generateVerbs() {
+
+    System.out.println("Generating Verbs.");
+
+    String[] positiveNegatives = {"", "Neg"};
+    String[] times = {"Imp", "Aor", "Past", "Prog1", "Prog2", "Narr", "Fut"};
+    String[] persons = {"A1sg", "A2sg", "A3sg", "A1pl","A2pl","A3pl"};
+
+    TurkishMorphology morphology =
+            TurkishMorphology.builder().setLexicon("okumak").disableCache().build();
+
+    for (String posNeg : positiveNegatives) {
+      for (String time : times) {
+        for (String person : persons) {
+          List<String> seq = Stream.of(posNeg, time, person)
+                  .filter(s->s.length()>0)
+                  .collect(Collectors.toList());
+
+          List<Result> results =
+                  morphology.getWordGenerator().generate("oku", seq);
           results.forEach(s->System.out.println(s.surface));
         }
       }
